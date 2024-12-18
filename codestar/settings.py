90607fe4a515401a2e29cba913ca839dc7556346
Lewis Wheeler <Lewisswheelerr@outlook.com>
2024-12-18 16:04:33 +0000

    Set DEBUG to False

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d(n2b35j=&)^t7vif4%!#+3nxct6js4(hs&1oqdth++oljl@5!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
