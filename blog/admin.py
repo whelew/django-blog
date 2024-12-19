891f6624e38c51c665252c4229ecd824dcda0276
Lewis Wheeler <Lewisswheelerr@outlook.com>
2024-12-19 14:47:43 +0000

    Added admin.site.register for comment in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Comment)
